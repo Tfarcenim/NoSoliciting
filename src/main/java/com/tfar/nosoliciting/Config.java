4d352fc58996da26cb8e0a051da2c165d7c8cda4
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2019-12-12 07:19:42 -0500

    1.1

---
+++ src/main/java/com/tfar/nosoliciting/Config.java
@@ -0,0 +1,28 @@
+package com.tfar.nosoliciting;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import org.apache.commons.lang3.tuple.Pair;
+
+public class Config {
+
+  public static ForgeConfigSpec.IntValue range;
+
+  public static final Config SERVER;
+  public static final ForgeConfigSpec SERVER_SPEC;
+
+  static {
+    final Pair<Config, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(Config::new);
+    SERVER_SPEC = specPair.getRight();
+    SERVER = specPair.getLeft();
+  }
+
+  Config(ForgeConfigSpec.Builder builder) {
+    builder.push("general");
+
+    range = builder
+            .comment("Range of effect in chunks, 1 = 3x3, 2 = 5x5, 3 = 7x7,etc")
+            .translation("text.nosoliciting.config.range")
+            .defineInRange("Chunk Range",2,0,Integer.MAX_VALUE);
+    builder.pop();
+  }
+}
--- src/main/java/com/tfar/nosoliciting/NoSoliciting.java
+++ src/main/java/com/tfar/nosoliciting/NoSoliciting.java
@@ -5,16 +5,25 @@ import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.merchant.villager.WanderingTraderEntity;
 import net.minecraft.tileentity.SignTileEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.IChunk;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.IntStream;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(NoSoliciting.MODID)
@@ -24,29 +33,46 @@ public class NoSoliciting {
   private static final Pattern pattern = Pattern.compile("(?i)no soliciting");
 
   public NoSoliciting() {
-    MinecraftForge.EVENT_BUS.addListener(this::onServerStarting);
+    ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, Config.SERVER_SPEC);
+    MinecraftForge.EVENT_BUS.addListener(this::wanderingTraderSpawn);
   }
 
   // You can use SubscribeEvent and let the Event Bus discover methods to call
   @SubscribeEvent
-  public void onServerStarting(LivingSpawnEvent.SpecialSpawn event) {
+  public void wanderingTraderSpawn(LivingSpawnEvent.SpecialSpawn event) {
     LivingEntity entity = event.getEntityLiving();
     if (!(entity instanceof WanderingTraderEntity) || event.getSpawnReason() != SpawnReason.NATURAL) return;
     World world = entity.world;
     BlockPos eventPos = entity.getPosition();
-    BlockPos startPos = eventPos.add(-32, -32, -32);
-    BlockPos endPos = eventPos.add(32, 32, 32);
+
+    IChunk eventChunk = world.getChunk(eventPos);
+
+    int radius = Config.range.get();
+
+    ChunkPos chunkPos = eventChunk.getPos();
+
+    int startX = chunkPos.x - radius;
+    int startZ = chunkPos.z - radius;
+    int endX = chunkPos.x + radius;
+    int endZ = chunkPos.z + radius;
+    ArrayList<IChunk> chunks = new ArrayList<>();
+
+    IntStream.range(startZ, endZ)
+            .forEach(z ->
+                    IntStream.range(startX, endX)
+                            .forEach(x -> chunks.add(world.getChunk(x, z))));
+
+
     event.setCanceled(
-            BlockPos.getAllInBox(startPos, endPos)
-            .filter(pos -> world.getTileEntity(pos) instanceof SignTileEntity)
-            .anyMatch(pos -> {
-              SignTileEntity sign = (SignTileEntity) world.getTileEntity(pos);
-              return Arrays
-                      .stream(sign.signText)
-                      .map(ITextComponent::getString)
-                      .map(pattern::matcher)
-                      .anyMatch(Matcher::matches);
-            })
+            chunks
+                    .stream()
+                    .filter(Chunk.class::isInstance)
+                    .flatMap(c -> ((Chunk) c).getTileEntityMap().values().stream())
+                    .filter(SignTileEntity.class::isInstance)
+                    .flatMap(tileEntity -> Arrays.stream(((SignTileEntity) tileEntity).signText))
+                    .map(ITextComponent::getString)
+                    .map(pattern::matcher)
+                    .anyMatch(Matcher::matches)
     );
   }
 }
